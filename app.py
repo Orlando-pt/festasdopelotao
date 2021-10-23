a11350e9ae547e6587d02e5a936d85d2d5dc3492
Orlando-pt <orlando.macedo15@ua.pt>
2021-10-23 15:54:57 +0100

    added migrations

---
+++ app.py
@@ -0,0 +1,77 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+
+app = Flask(__name__)
+
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# models
+class Book(db.Model):
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    author = db.Column(db.String())
+    published = db.Column(db.String())
+
+    def __init__(self, name, author, published):
+        self.name = name
+        self.author = author
+        self.published = published
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+    
+    def serialize(self):
+        return {
+            'id': self.id, 
+            'name': self.name,
+            'author': self.author,
+            'published':self.published
+        }
+
+# api
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+@app.route("/add")
+def add_book():
+    name=request.args.get('name')
+    author=request.args.get('author')
+    published=request.args.get('published')
+    try:
+        book=Book(
+            name=name,
+            author=author,
+            published=published
+        )
+        db.session.add(book)
+        db.session.commit()
+        return "Book added. book id={}".format(book.id)
+    except Exception as e:
+	    return(str(e))
+
+@app.route("/getall")
+def get_all():
+    try:
+        books=Book.query.all()
+        return  jsonify([e.serialize() for e in books])
+    except Exception as e:
+	    return(str(e))
+
+@app.route("/get/<id_>")
+def get_by_id(id_):
+    try:
+        book=Book.query.filter_by(id=id_).first()
+        return jsonify(book.serialize())
+    except Exception as e:
+	    return(str(e))
+
+if __name__ == '__main__':
+    app.run()
--- config.py
+++ config.py
@@ -1,12 +1,11 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SECRET_KEY = 'c0257b78bde6ce8a04a092cf5d4e6602611fd27d35a439c0b72dcc6b97df1d1b'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
